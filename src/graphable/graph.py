0da2ec4e1e787ec91003f80fc5674e655965cbc2
Richard West <dopplereffect.us@gmail.com>
2026-01-19 14:43:59 -0800

    add tags and tweaking methods

--- src/graphable/graph.py
+++ src/graphable/graph.py
@@ -6,6 +6,31 @@ from typing import Any, Callable
 from .graphable import Graphable
 
 
+def graph[T: Graphable[Any, Any]](contains: list[T]) -> Graph[T]:
+    def go_down(node: T, nodes: set[T]) -> None:
+        for down_node in node.dependents:
+            if down_node in nodes:
+                continue
+
+            nodes.add(down_node)
+            go_down(down_node, nodes)
+
+    def go_up(node: T, nodes: set[T]) -> None:
+        for up_node in node.depends_on:
+            if up_node in nodes:
+                continue
+
+            nodes.add(up_node)
+            go_up(up_node, nodes)
+
+    nodes: set[T] = set(contains)
+    for node in contains:
+        go_up(node, nodes)
+        go_down(node, nodes)
+
+    return Graph(nodes)
+
+
 class Graph[T: Graphable[Any, Any]]:
     def __init__(self, initial: set[T] | None = None):
         self._nodes: set[T] = initial if initial else set[T]()
@@ -17,9 +42,6 @@ class Graph[T: Graphable[Any, Any]]:
         if self._topological_order is not None:
             self._topological_order = None
 
-    def filtered_topological_order(self, fn: Callable[[T], bool]) -> list[T]:
-        return [node for node in self.topological_order() if fn(node)]
-
     @property
     def sinks(self) -> list[T]:
         return [node for node in self._nodes if 0 == len(node.dependents)]
@@ -28,29 +50,11 @@ class Graph[T: Graphable[Any, Any]]:
     def sources(self) -> list[T]:
         return [node for node in self._nodes if 0 == len(node.depends_on)]
 
-    def subgraph(self, contains: set[T]) -> Graph[T]:
-        def go_down(node: T, nodes: set[T]) -> None:
-            for down_node in node.dependents:
-                if down_node in nodes:
-                    continue
+    def subgraph_filtered(self, fn: Callable[[T], bool]) -> Graph[T]:
+        return graph([node for node in self._nodes if fn(node)])
 
-                nodes.add(down_node)
-                go_down(down_node, nodes)
-
-        def go_up(node: T, nodes: set[T]) -> None:
-            for up_node in node.depends_on:
-                if up_node in nodes:
-                    continue
-
-                nodes.add(up_node)
-                go_up(up_node, nodes)
-
-        nodes: set[T] = set(contains)
-        for node in contains:
-            go_up(node, nodes)
-            go_down(node, nodes)
-
-        return Graph(nodes)
+    def subgraph_tagged(self, tag: str) -> Graph[T]:
+        return graph([node for node in self._nodes if node.is_tagged(tag)])
 
     def topological_order(self) -> list[T]:
         if self._topological_order is None:
@@ -58,3 +62,9 @@ class Graph[T: Graphable[Any, Any]]:
             self._topological_order = list(sorter.static_order())
 
         return self._topological_order
+
+    def topological_order_filtered(self, fn: Callable[[T], bool]) -> list[T]:
+        return [node for node in self.topological_order() if fn(node)]
+
+    def topological_order_tagged(self, tag: str) -> list[T]:
+        return [node for node in self.topological_order() if node.is_tagged(tag)]
--- src/graphable/graphable.py
+++ src/graphable/graphable.py
@@ -6,12 +6,16 @@ class Graphable[T, S: Graphable[Any, Any]]:
         self._dependents: set[S] = set()
         self._depends_on: set[S] = set()
         self._reference: T = reference
+        self._tags: set[str] = set()
 
     def _add_dependent(self, dependent: S) -> None:
         self._dependents.add(dependent)
 
     def _add_depends_on(self, depends_on: S) -> None:
         self._depends_on.add(depends_on)
+
+    def add_tag(self, tag: str) -> None:
+        self._tags.add(tag)
 
     @property
     def dependents(self) -> set[S]:
@@ -21,6 +25,12 @@ class Graphable[T, S: Graphable[Any, Any]]:
     def depends_on(self) -> set[S]:
         return set(self._depends_on)
 
+    def is_tagged(self, tag: str) -> bool:
+        return tag in self._tags
+
     @property
     def reference(self) -> T:
         return self._reference
+
+    def remove_tag(self, tag: str) -> None:
+        self._tags.discard(tag)
